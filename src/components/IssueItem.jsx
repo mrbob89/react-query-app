2d350c90541d5b7a8b85c342c3eef743bbd47cbb
Roman Boboshko <roman.boboshko@gdxtec.com>
2023-02-09 00:41:26 +0200

    Updated IssueItem component. fetched users

--- src/components/IssueItem.jsx
+++ src/components/IssueItem.jsx
@@ -1,37 +1,58 @@
 import { Link } from "react-router-dom";
 import { GoIssueOpened, GoIssueClosed, GoComment } from 'react-icons/go';
 import { relativeDate } from '../helpers/relativeDate';
+import { useUserData } from "../helpers/useUserData";
 
 export function IssueItem({
-  title, number, assignee, commentCount, createdBy, createdDate, labels, status,
+  title,
+  number,
+  assignee,
+  commentCount,
+  createdBy,
+  createdDate,
+  labels,
+  status,
 }) {
-  return <li>
-    <div>
-      {status === 'done' || status === 'canceled' ? (
-        <GoIssueClosed style={{ color: "red" }} />
-      ) : (
-        <GoIssueOpened style={{ color: "green" }} />
-      )}
-    </div>
-    <div className="issue-content">
-      <span>
-        <Link to={`issue/${number}`}>{title}</Link>
-        {labels.map(label => (
-          <span key={label} className={`label red`}>
-            {label}
-          </span>
-        ))}
+  const assigneeUser = useUserData(assignee)
+  const createdByUser = useUserData(createdBy)
+
+  return (
+    <li>
+      <div>
+        {status === 'done' || status === 'canceled' ? (
+          <GoIssueClosed style={{ color: "red" }} />
+        ) : (
+          <GoIssueOpened style={{ color: "green" }} />
+        )}
+      </div>
+      <div className="issue-content">
+        <span>
+          <Link to={`issue/${number}`}>{title}</Link>
+          {labels.map(label => (
+            <span key={label} className={`label red`}>
+              {label}
+            </span>
+          ))}
+        </span>
+        <small>
+          #{number} opened {relativeDate(createdDate)}{" "}
+          {createdByUser.isSuccess ? `by ${createdByUser.data.name}` : ""}
+        </small>
+      </div>
+      {assignee ?
+        <img
+          src={assigneeUser.isSuccess ? assigneeUser.data.profilePictureUrl : ""}
+          className="assigned-to"
+          alt={assigneeUser.isSuccess ? `Assigned to ${assigneeUser.data.name}` : 'Avatar'}
+        /> : null}
+      <span className="comment-count">
+        {commentCount > 0 ? (
+          <>
+            <GoComment /> 
+            {commentCount}
+          </>
+        ) : null}
       </span>
-      <small>#{number} opened {relativeDate(createdDate)} by {createdBy}</small>
-    </div>
-    {assignee ? <div>{assignee}</div> : null}
-    <span className="comment-count">
-      {commentCount > 0 ? (
-        <>
-          <GoComment />
-          {commentCount}
-        </>
-      ) : null}
-    </span>
-  </li>;
+    </li>
+  );
 }
